a6f33813c1cad7077501093e68ebbacd17c11499
Nazran Khondokar <nazran@rokomari.com>
2020-03-05 17:52:52 +0600

    Initial Commit

---
+++ src/test/java/com/nazran/springsecurityjwt/SpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nazran.springsecurityjwt;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
